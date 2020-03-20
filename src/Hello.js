1cdfa9ad359b1d5ff1b7d4541778ff739b1f7157
Matt Kim <mattsunwoongkim@gmail.com>
2020-03-19 22:56:31 -0700

    Initial Hello.js

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+function Hello (){
+   
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to React</p>
+    </div>
+  );
+  
+}
+
+export default Hello;
