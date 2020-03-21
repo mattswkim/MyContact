2eebd9ad8bb4f5fbb311f73e9ac9db4f851a2983
Matt Kim <mattsunwoongkim@gmail.com>
2020-03-21 16:20:37 -0700

    random cat contacts are added

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = ({
+    name,
+    email,
+    id
+  }) => {
+  
+  return (
+    <div className='tc bg-washed-blue dib br3 pa3 mr2 grow bw2 shadow-5'>
+      <img alt='cats' src={`https://robohash.org/${id}?set=set4&size=200x200`}></img>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
