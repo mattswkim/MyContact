5540e71b6afff67438f4e02cda2e84b31711edf6
Matt Kim <mattsunwoongkim@gmail.com>
2020-03-23 16:39:48 -0700

    Updates

---
+++ precache-manifest.be7b8ded2e46ce14b120cd28df86c7ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bfab9e2f9c938caef839cd3a02b5c0",
+    "url": "/MyContact/index.html"
+  },
+  {
+    "revision": "684a6276fecebdf6b34a",
+    "url": "/MyContact/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f52cf167c3594fdf818f",
+    "url": "/MyContact/static/css/main.2ef557da.chunk.css"
+  },
+  {
+    "revision": "684a6276fecebdf6b34a",
+    "url": "/MyContact/static/js/2.7067edd8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MyContact/static/js/2.7067edd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52cf167c3594fdf818f",
+    "url": "/MyContact/static/js/main.0453b87c.chunk.js"
+  },
+  {
+    "revision": "2adf22416307f15919ab",
+    "url": "/MyContact/static/js/runtime-main.3c88e5e7.js"
+  },
+  {
+    "revision": "cca9b1336751eb6dedb5c2c97b8d8825",
+    "url": "/MyContact/static/media/MOONYCAT.cca9b133.woff"
+  }
+]);
