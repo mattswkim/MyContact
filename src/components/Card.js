694654cb188b0dbd471ac0c94a58d894f7f2b481
Matt Kim <mattsunwoongkim@gmail.com>
2020-03-29 23:27:57 -0700

    cleaning v1

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Card = ({
     name,
-    email,
-    id
+    email
   }) => {
   
   return (
--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -9,8 +9,6 @@ const Cardlist = ({ cats }) => {
         cats.map((user, i) => {
           return (
             <Card 
-              key={i} 
-              id={cats[i].id} 
               name={cats[i].name} 
               email={cats[i].email}
             />
